62d54d70e8ab58e877cc85bfc07e99e60bf60cbf
Stanislava <stasya@guseva.spb.ru>
2024-10-12 23:21:02 +0300

    Добавила случай, когда канал скомпрометирован Евой в протокол bb84

--- bb84.py
+++ bb84.py
@@ -44,47 +44,101 @@ class BB84():
     @staticmethod
     def send_single_bit_with_bb84(
             your_device: QuantumDevice,
-            eve_device: QuantumDevice
+            alice_device: QuantumDevice
         ) -> tuple:
 
         [your_message, your_basis] = [
             BB84.sample_random_bit(your_device) for _ in range(2)
         ]
 
+        alice_basis = BB84.sample_random_bit(alice_device)
+
+        with your_device.using_qubit() as q:
+            BB84.prepare_message_qubit(your_message, your_basis, q)
+            # ОТПРАВКА КУБИТА...
+            alice_result = BB84.measure_message_qubit(alice_basis, q)
+
+        return ((your_message, your_basis), (alice_result, alice_basis))
+
+    @staticmethod
+    def send_single_bit_with_eve_interception(
+            your_device: QuantumDevice,
+            eve_device: QuantumDevice,
+            alice_device: QuantumDevice
+    ) -> tuple:
+
+        # Боб выбирает случайное сообщение и базис
+        [your_message, your_basis] = [
+            BB84.sample_random_bit(your_device) for _ in range(2)
+        ]
+
+        # Ева перехватывает сообщение
         eve_basis = BB84.sample_random_bit(eve_device)
 
         with your_device.using_qubit() as q:
             BB84.prepare_message_qubit(your_message, your_basis, q)
             # ОТПРАВКА КУБИТА...
+
+            # Ева измеряет кубит в своем случайном базисе
             eve_result = BB84.measure_message_qubit(eve_basis, q)
 
-        return ((your_message, your_basis), (eve_result, eve_basis))
+            # Ева "восстанавливает" кубит по своему результату
+            BB84.prepare_message_qubit(eve_result, eve_basis, q)
+
+            # Ева отправляет кубит Алисе
+            alice_basis = BB84.sample_random_bit(alice_device)
+            alice_result = BB84.measure_message_qubit(alice_basis, q)
+
+        return ((your_message, your_basis), (eve_result, eve_basis), (alice_result, alice_basis))
 
     """
-    протокол BB84 для обмена ключом с Евой
+    протокол BB84 для обмена ключом с Алисой
     """
 
     @staticmethod
     def simulate_bb84(n_bits: int) -> tuple:
         your_device = SingleQubitSimulator()
-        eve_device = SingleQubitSimulator()
+        alice_device = SingleQubitSimulator()
 
         key = []
         n_rounds = 0
         while len(key) < n_bits:
             n_rounds += 1
-            ((your_message, your_basis), (eve_result, eve_basis)) = \
-                (BB84.send_single_bit_with_bb84(your_device, eve_device))
+            ((your_message, your_basis), (alice_result, alice_basis)) = \
+                (BB84.send_single_bit_with_bb84(your_device, alice_device))
 
-            if your_basis == eve_basis:
-                assert your_message == eve_result #проверка на прослушку
+            if your_basis == alice_basis:
+                assert your_message == alice_result #проверка на прослушку
                 key.append(your_message)
 
         print(f"Потребовалось {n_rounds} раундов, чтобы сгенерировать {n_bits} - битовый ключ.")
         return key
 
+    @staticmethod
+    def simulate_bb84_with_eve(n_bits: int) -> tuple:
+        your_device = SingleQubitSimulator()
+        eve_device = SingleQubitSimulator()
+        alice_device = SingleQubitSimulator()
+
+        key = []
+        n_rounds = 0
+        while len(key) < n_bits:
+            n_rounds += 1
+            ((your_message, your_basis), (eve_result, eve_basis), (alice_result, alice_basis)) = \
+                (BB84.send_single_bit_with_eve_interception(your_device, eve_device, alice_device))
+
+            if your_basis == alice_basis:
+                # Проверка на вмешательство Евы: если базисы совпадают, сообщение должно быть правильным
+                if your_message != alice_result:
+                    print(f"Вмешательство Евы! Значение искажено.")
+                else:
+                    key.append(your_message)
+
+        print(f"Потребовалось {n_rounds} раундов, чтобы сгенерировать {n_bits} - битовый ключ.")
+        return key
+
     """
-    протокол BB84 для обмена ключом с Евой
+    протокол BB84 для обмена ключом с Алисой
     """
 
     def apply_one_time_pad(message: List[bool], key: List[bool]) -> List[bool]: #шифрование
--- run_bb84.py
+++ run_bb84.py
@@ -22,4 +22,32 @@ if __name__ == "__main__":
     encrypted_message = BB84.apply_one_time_pad(message, key)
     print(f"Зашифрованное сообщение: {BB84.convert_to_hex(encrypted_message)}.")
     decrypted_message = BB84.apply_one_time_pad(encrypted_message, key)
-    print(f"Ева расшифровала, получив: {BB84.convert_to_hex(decrypted_message)}.")
+    print(f"Алиса расшифровала, получив: {BB84.convert_to_hex(decrypted_message)}.")
+
+
+    print("\n\n\n\n")
+
+
+    print("Генерирование 96-битового ключа с перехватом Евой...")
+    key = BB84.simulate_bb84_with_eve(96)
+    print(f"Получен ключ {BB84.convert_to_hex(key)}.")
+
+    message = [
+        0, 0, 1, 0, 0, 1, 1, 1,
+        1, 1, 0, 0, 0, 0, 1, 0,
+        0, 0, 1, 0, 0, 0, 1, 1,
+        0, 1, 1, 0, 1, 0, 0, 1,
+        0, 0, 1, 0, 0, 1, 1, 1,
+        1, 1, 0, 0, 0, 0, 1, 0,
+        0, 0, 1, 0, 0, 0, 1, 1,
+        1, 1, 1, 1, 0, 0, 1, 0,
+        0, 0, 1, 0, 0, 1, 1, 1,
+        1, 1, 0, 0, 0, 0, 1, 0,
+        0, 0, 1, 0, 0, 0, 1, 1,
+        0, 1, 0, 0, 0, 1, 0, 0
+    ]
+    print(f"Использование ключа для отправки секретного сообщения: {BB84.convert_to_hex(message)}.")
+    encrypted_message = BB84.apply_one_time_pad(message, key)
+    print(f"Зашифрованное сообщение: {BB84.convert_to_hex(encrypted_message)}.")
+    decrypted_message = BB84.apply_one_time_pad(encrypted_message, key)
+    print(f"Расшифрованное сообщение: {BB84.convert_to_hex(decrypted_message)}.")
